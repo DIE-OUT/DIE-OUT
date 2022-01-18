61818867f6f1be1f2a9003b526dc3097fafb0429
Afired <conor.huismann@gmail.com>
2022-01-18 12:42:57 +0100

    added sessionbuilder helper factory to create a session

--- Assets/src/internal/Sessions/Session.cs
+++ Assets/src/internal/Sessions/Session.cs
@@ -4,7 +4,22 @@ namespace DieOut.Sessions {
     
     [Serializable]
     public class Session {
-        public int something;
+
+        public int PlayerCount => Player.Length;
+        public Player[] Player { get; }
+        public int MaxRounds { get; }
+        public int WinningScore { get; }
+        
+        public int CurrentRound { get; private set; }
+        
+        public Session(Player[] player, int maxRounds, int winningScore) {
+            Player = player;
+            MaxRounds = maxRounds;
+            winningScore = WinningScore;
+
+            CurrentRound = 0;
+        }
+        
     }
     
 }
---
+++ Assets/src/internal/Sessions/SessionBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DieOut.Sessions {
+    
+    public class SessionBuilder {
+        
+        public int PlayerCount { get; set; }
+        public GameMode.GameMode ActivatedGameModes { get; set; } = GameMode.GameMode.Dornenkrone;
+        public int MaxRounds { get; set; } = 5;
+        public int WinningScore { get; set; } = 3;
+
+        public bool IsValid() {
+            if(MaxRounds < 1) return false;
+            if(PlayerCount < 2) return false;
+            if(ActivatedGameModes == 0) return false;
+            return true;
+        }
+        
+        public Session Create() {
+            if(!IsValid())
+                throw new Exception("Session Builder is invalid or incomplete - check before creating");
+            
+            Player[] players = new Player[PlayerCount];
+            for(int i = 0; i < PlayerCount; i++) {
+                players[i] = new Player();
+            }
+            
+            return new Session(players, MaxRounds, WinningScore);
+        }
+        
+    }
+    
+}
