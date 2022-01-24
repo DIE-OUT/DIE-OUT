e087ed75a9fb3bf97e675f03933e7d85de1bf5d5
Afired <conor.huismann@gmail.com>
2022-01-24 09:55:11 +0100

    for now last change on settings screen

--- Assets/src/internal/UI/MainMenu/SettingsScreen.cs
+++ Assets/src/internal/UI/MainMenu/SettingsScreen.cs
@@ -11,6 +11,8 @@ namespace DieOut.UI.MainMenu {
         
         [Header("UI References")]
         [SerializeField] private Switcher _masterAudioSwitcher;
+        [SerializeField] private Switcher _musicAudioSwitcher;
+        [SerializeField] private Switcher _effectsAudioSwitcher;
         [SerializeField] private Switcher _windowModeSwitcher;
         [SerializeField] private Switcher _graphicsQualityLevelSwitcher;
         
@@ -19,6 +21,14 @@ namespace DieOut.UI.MainMenu {
             ISwitchControl masterAudioSwitchControl = new RangedIntSwitchControl(new RangedIntSwitchControl.Range<int>(0, 10), 10);
             masterAudioSwitchControl.OnValueChanged += (value, _) => AudioManager.SetVolume(AudioChannel.Master, (int) value * 0.1f);
             _masterAudioSwitcher.AssignControl(masterAudioSwitchControl);
+            
+            ISwitchControl musicAudioSwitchControl = new RangedIntSwitchControl(new RangedIntSwitchControl.Range<int>(0, 10), 10);
+            musicAudioSwitchControl.OnValueChanged += (value, _) => AudioManager.SetVolume(AudioChannel.Music, (int) value * 0.1f);
+            _musicAudioSwitcher.AssignControl(musicAudioSwitchControl);
+            
+            ISwitchControl effectsAudioSwitchControl = new RangedIntSwitchControl(new RangedIntSwitchControl.Range<int>(0, 10), 10);
+            effectsAudioSwitchControl.OnValueChanged += (value, _) => AudioManager.SetVolume(AudioChannel.Effects, (int) value * 0.1f);
+            _effectsAudioSwitcher.AssignControl(effectsAudioSwitchControl);
             
             ISwitchControl windowModeSwitchControl = new EnumSwitchControl<WindowMode>(WindowManager.CurrentWindowMode);
             windowModeSwitchControl.OnValueChanged += (value, _) => WindowManager.SetWindowMode((WindowMode) value);
