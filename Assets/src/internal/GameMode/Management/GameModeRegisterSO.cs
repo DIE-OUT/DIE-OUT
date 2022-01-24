916dfe1c2bb02dd00b889186fff3123d975011f5
Afired <conor.huismann@gmail.com>
2022-01-24 13:15:48 +0100

    added game mode, game mode register so scriptable objects and changed internal apis

---
+++ Assets/src/internal/GameMode/Management/GameModeRegisterSO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DieOut.GameMode.Management {
+    
+    [CreateAssetMenu]
+    public class GameModeRegisterSO : ScriptableObject {
+
+        [SerializeField] public List<GameMode> GameModes;
+
+    }
+    
+}
