2e9244a8f8db56d6225440f0c38b2d6430fca016
Afired <conor.huismann@gmail.com>
2022-01-27 14:04:00 +0100

    added playtest level select

---
+++ Assets/src/internal/PlayTestLevelSelect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DieOut {
+    
+    public class PlayTestLevelSelect : MonoBehaviour {
+
+        [SerializeField] private SceneField[] _playtestScenes;
+        
+        public void LoadLevel(int index) {
+            SceneManager.LoadScene(_playtestScenes[index].SceneName);
+        }
+        
+    }
+    
+}
