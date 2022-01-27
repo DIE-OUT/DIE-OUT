6863c115041a77f497e67033fd957b6559251feb
Afired <conor.huismann@gmail.com>
2022-01-27 15:51:43 +0100

    added back to level select button and added win canvas to all test maps

--- Assets/src/internal/GameMode/Dornenkrone/Win.cs
+++ Assets/src/internal/GameMode/Dornenkrone/Win.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using DieOut.GameMode.Interactions;
+using DieOut.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,16 +11,26 @@ using UnityEngine.UI;
 namespace DieOut.GameMode.Dornenkrone {
     public class Win : MonoBehaviour {
         // ! statt [SerializeField] sollten alle Objects in der Szene mit dem type of Movable automatisch gefunden und in die Liste gefügt werden
-        [SerializeField] private List<Movable> _players;
-        
+        private List<Movable> _players;
+        [SerializeField] private SceneField _levelSelectScene;
+
+
+        private void Awake() {
+            _players = FindObjectsOfType<Movable>().ToList();
+        }
+
         private void Update() {
             if (CheckHealth()) {
-                transform.GetChild(2).gameObject.SetActive(true);
+                transform.GetChild(0).gameObject.SetActive(true);
             }
         }
 
         private bool CheckHealth() {
             return _players.Any(player => player._health <= 0);
+        }
+
+        public void LoadLevelSelect() {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(_levelSelectScene.SceneName);
         }
     }
 }
