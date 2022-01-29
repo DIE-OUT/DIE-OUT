e72db9d6adbd5ff2b68f1740d9939cb707e5faa2
Afired <conor.huismann@gmail.com>
2022-01-29 14:36:44 +0100

    basic player spawner with spawnpoints

---
+++ Assets/src/internal/GameMode/Dornenkrone/DornenkronePlayerSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DieOut.GameMode.Management;
+using DieOut.Sessions;
+using UnityEngine;
+
+namespace DieOut.GameMode.Dornenkrone {
+    
+    public class DornenkronePlayerSpawner : PlayerSpawner {
+
+        [SerializeField] private GameObject _dornenkronePlayerPrefab;
+        
+        
+        protected override void OnGameModePrepare(Player[] players, PlayerSpawnpoint[] playerSpawnpoints) {
+            for(int i = 0; i < players.Length; i++) {
+                GameObject player = Instantiate(_dornenkronePlayerPrefab, playerSpawnpoints[i].transform.position, Quaternion.identity);
+                //todo: more initialization
+            }
+        }
+        
+    }
+    
+}
---
+++ Assets/src/internal/GameMode/Management/PlayerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DieOut.Sessions;
+using UnityEngine;
+
+namespace DieOut.GameMode.Management {
+    
+    public abstract class PlayerSpawner : MonoBehaviour {
+        
+        private void Awake() {
+            Sessions.Sessions.Current.OnGameModePrepare += InvokeGameModePrepare;
+        }
+
+        private void InvokeGameModePrepare() {
+            PlayerSpawnpoint[] playerSpawnpoints = FindObjectsOfType<PlayerSpawnpoint>();
+            Player[] players = Sessions.Sessions.Current.Player;
+            
+            if(players.Length > playerSpawnpoints.Length)
+                throw new Exception("map has less player spawns than players that are playing!");
+            
+            OnGameModePrepare(players, playerSpawnpoints);
+        }
+
+        protected abstract void OnGameModePrepare(Player[] players, PlayerSpawnpoint[] playerSpawnpoints);
+        
+    }
+    
+}
