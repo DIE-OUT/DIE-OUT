5280459fcfd8420c6c1b1c2ed8912c674800feca
Afired <conor.huismann@gmail.com>
2022-02-03 16:35:00 +0100

    added functionaltity of any button pressed

--- Assets/src/internal/DieOut/UI/LoadingScreen/LoadingScreenOnlyContinueWithConfirmation.cs
+++ Assets/src/internal/DieOut/UI/LoadingScreen/LoadingScreenOnlyContinueWithConfirmation.cs
@@ -8,13 +8,13 @@ using UnityEngine;
 namespace DieOut.UI.LoadingScreen {
     
     public class LoadingScreenOnlyContinueWithConfirmation : MonoBehaviour {
-
-        [SerializeField] private GameObject _confirmationButtonRoot;
+        
+        [SerializeField] private GameObject _gameObjectToBeEnabled;
         private bool _confirmed;
-
-
+        
+        
         private void Awake() {
-            _confirmationButtonRoot.SetActive(false);
+            _gameObjectToBeEnabled.SetActive(false);
             
             // dont require confirmation if not loading into a game mode
             if(!Session.HasCurrent || Session.Current.GameModeInstance == null) {
@@ -25,14 +25,19 @@ namespace DieOut.UI.LoadingScreen {
         }
 
         private Task EnableConfirmationButton() {
-            _confirmationButtonRoot.SetActive(true);
+            _gameObjectToBeEnabled.SetActive(true);
             return Task.CompletedTask;
         }
         
         private async Task WaitForConfirmation() {
+            InputTable inputTable = new InputTable();
+            inputTable.Enable();
+            inputTable.Navigation.AnyButton.performed += (_) => _confirmed = true;
             while(!_confirmed) {
                 await Await.NextUpdate();
             }
+            inputTable.Disable();
+            inputTable.Dispose();
         }
         
         [Button]
