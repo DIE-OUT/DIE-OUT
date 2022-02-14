df2df325864a053e73c794fda30ba5994b6825b3
Afired <conor.huismann@gmail.com>
2022-02-14 18:10:36 +0100

    working model preview with render textures in character select screen

--- Assets/src/internal/DieOut/UI/CharacterSelect/CharacterSelectCard.cs
+++ Assets/src/internal/DieOut/UI/CharacterSelect/CharacterSelectCard.cs
@@ -2,6 +2,7 @@
 using Afired.UI.Elements;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 namespace DieOut.UI.CharacterSelect {
     
@@ -9,6 +10,9 @@ namespace DieOut.UI.CharacterSelect {
     public class CharacterSelectCard : MonoBehaviour {
         
         [SerializeField] private Switcher _colorSwitcher;
+        [SerializeField] private ModelPreviewManager _modelPreviewManager;
+        [SerializeField] private RawImage _renderTextureDisplay;
+        [SerializeField] private Camera _renderTextureCamera;
         [SerializeField] private GameObject _gameObjectToActivateWhenControlAssigned;
         [SerializeField] private GameObject _gameObjectToDeactivateWhenControlAssigned;
         public bool IsAssigned => _inputDevice != null;
@@ -16,6 +20,7 @@ namespace DieOut.UI.CharacterSelect {
         private InputDevice _inputDevice;
         [SerializeField] public PlayerColor PlayerColor;
         private ISwitchControl _colorSwitchControl;
+        private RenderTexture _renderTexture;
         
         private void Awake() {
             _assignUIControl = GetComponent<AssignUIControl>();
@@ -24,7 +29,13 @@ namespace DieOut.UI.CharacterSelect {
             
             _colorSwitchControl = new EnumSwitchControl<PlayerColor>(PlayerColor);
             _colorSwitchControl.OnValueChanged += (value, valueAsText) => PlayerColor = (PlayerColor) value;
+            _colorSwitchControl.OnValueChanged += (value, valueAsText) => _modelPreviewManager.Refresh((PlayerColor) value);
             _colorSwitcher.AssignControl(_colorSwitchControl);
+
+            _renderTexture = new RenderTexture(256, 512, 16, RenderTextureFormat.ARGB32);
+            _renderTexture.Create();
+            _renderTextureDisplay.texture = _renderTexture;
+            _renderTextureCamera.targetTexture = _renderTexture;
         }
         
         public void AssignDevice(InputDevice inputDevice) {
@@ -33,10 +44,15 @@ namespace DieOut.UI.CharacterSelect {
             
             _gameObjectToActivateWhenControlAssigned.SetActive(true);
             _gameObjectToDeactivateWhenControlAssigned.SetActive(false);
+            _modelPreviewManager.Refresh((PlayerColor) _colorSwitchControl.GetValue());
         }
 
         private void OnDisable() {
             Reset();
+        }
+
+        private void OnDestroy() {
+            _renderTexture.DiscardContents();
         }
 
         private void Reset() {
@@ -46,6 +62,7 @@ namespace DieOut.UI.CharacterSelect {
             _gameObjectToActivateWhenControlAssigned.SetActive(false);
             _gameObjectToDeactivateWhenControlAssigned.SetActive(true);
             _assignUIControl.DeactivateInput();
+            _modelPreviewManager.Refresh(null);
         }
         
     }
