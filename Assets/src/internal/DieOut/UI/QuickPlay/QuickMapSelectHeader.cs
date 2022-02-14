07100f6a7afcaf774bdeb065d282989e92556043
Afired <conor.huismann@gmail.com>
2022-02-14 15:14:55 +0100

    added quick play

---
+++ Assets/src/internal/DieOut/UI/QuickPlay/QuickMapSelectHeader.cs
@@ -0,0 +1,17 @@
+﻿using Afired.GameManagement.GameModes;
+using TMPro;
+using UnityEngine;
+
+namespace DieOut.UI.QuickPlay {
+    
+    public class QuickMapSelectHeader : MonoBehaviour {
+
+        [SerializeField] private TMP_Text _headerTextElement;
+        
+        public void Init(GameMode gameMode) {
+            _headerTextElement.text = gameMode.DisplayName;
+        }
+        
+    }
+    
+}
---
+++ Assets/src/internal/DieOut/UI/QuickPlay/QuickMapSelectList.cs
@@ -0,0 +1,24 @@
+﻿using Afired.GameManagement.GameModes;
+using UnityEngine;
+
+namespace DieOut.UI.QuickPlay {
+    
+    public class QuickMapSelectList : MonoBehaviour {
+        
+        [SerializeField] private GameObject _quickMapSelectHeaderPrefab;
+        [SerializeField] private GameObject _quickMapSelectButtonPrefab;
+
+        private void Awake() {
+
+            foreach(GameMode gameMode in GameModeRegister.GameModes) {
+                Instantiate(_quickMapSelectHeaderPrefab, transform).GetComponent<QuickMapSelectHeader>().Init(gameMode);
+                foreach(Map map in gameMode.Maps) {
+                    Instantiate(_quickMapSelectButtonPrefab, transform).GetComponent<QuickMapSelectButton>().Init(gameMode, map);
+                }
+            }
+            
+        }
+        
+    }
+    
+}
