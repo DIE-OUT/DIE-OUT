3c9486d9c4270c4f4c55727aab9a3f8983565a9b
Afired <conor.huismann@gmail.com>
2022-02-15 00:49:23 +0100

    added character scriptable objects

---
+++ Assets/src/internal/GameManagement/Session/Character.cs
@@ -0,0 +1,19 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Afired.GameManagement.Sessions {
+    
+    [CreateAssetMenu]
+    public class Character : ScriptableObject {
+        
+        [SerializeField] private string _displayName;
+        [PreviewField] [SerializeField] private GameObject _model;
+        [SerializeField] private Avatar _avatar;
+        
+        public string DisplayName => _displayName;
+        public GameObject Model => _model;
+        public Avatar Avatar => _avatar;
+        
+    }
+    
+}
