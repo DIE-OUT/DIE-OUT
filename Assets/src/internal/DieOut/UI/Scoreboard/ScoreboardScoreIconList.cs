94d7aa44b2b8333e484a90174606d4a8a004eede
Afired <conor.huismann@gmail.com>
2022-02-18 17:27:28 +0100

    total points indication

--- Assets/src/internal/DieOut/UI/Scoreboard/ScoreboardScoreIconList.cs
+++ Assets/src/internal/DieOut/UI/Scoreboard/ScoreboardScoreIconList.cs
@@ -1,20 +1,21 @@
-﻿using System;
+﻿using Afired.GameManagement.Sessions;
 using UnityEngine;
 
 namespace DieOut.UI.Scoreboard {
     
     public class ScoreboardScoreIconList : MonoBehaviour {
 
-        [SerializeField] private GameObject _scoreIconPrefab;
+        [SerializeField] private GameObject _scoreIconPrefabTrue;
+        [SerializeField] private GameObject _scoreIconPrefabFalse;
 
         public void SetScore(int score) {
             
             for(int i = 0; i < transform.childCount; i++) {
                 Destroy(transform.GetChild(i).gameObject);
             }
-
-            for(int i = 0; i < score; i++) {
-                Instantiate(_scoreIconPrefab, transform);
+            
+            for(int i = 0; i < Session.Current.WinningScore; i++) {
+                Instantiate(i < score ? _scoreIconPrefabTrue : _scoreIconPrefabFalse, transform);
             }
             
         }
