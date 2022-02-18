52942eb8203e93341927c23b758f4acc83142ee0
Afired <conor.huismann@gmail.com>
2022-02-18 12:32:58 +0100

    fixed character 3d peview in character select screen

--- Assets/src/internal/DieOut/UI/CharacterSelect/ModelPreviewManager.cs
+++ Assets/src/internal/DieOut/UI/CharacterSelect/ModelPreviewManager.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using Afired.GameManagement.Characters;
+﻿using Afired.GameManagement.Characters;
 using Sirenix.OdinInspector;
-using Sirenix.Serialization;
 using UnityEngine;
 
 namespace DieOut.UI.CharacterSelect {
@@ -10,29 +8,22 @@ namespace DieOut.UI.CharacterSelect {
         
         [SerializeField] private float _rotationSpeed = 10f;
         [SerializeField] private GameObject _previewModelGameObjectNone;
-//        [OdinSerialize] private Dictionary<PlayerColor, GameObject> _modelPreviewPrefabsGameObjects = new Dictionary<PlayerColor, GameObject>();
+        private GameObject _proceduralSpawnedPreviewModel;
         
         private void Update() {
             transform.Rotate(Vector3.up, Time.deltaTime * _rotationSpeed);
         }
 
         public void Refresh(Character character) {
-            GameObject previewModelPrefab;
-            if(character is null) {
-                previewModelPrefab = _previewModelGameObjectNone;
-            } else {
-                //_modelPreviewPrefabsGameObjects.TryGetValue((Character)character, out previewModelPrefab);
-                previewModelPrefab = null;
-                //todo: spawn actual model
-            }
-            
+            if(_proceduralSpawnedPreviewModel != null)
+                Destroy(_proceduralSpawnedPreviewModel);
             _previewModelGameObjectNone.SetActive(false);
-//            foreach(GameObject modelPreviewGameObject in _modelPreviewPrefabsGameObjects.Values) {
-//                modelPreviewGameObject.SetActive(false);
-//            }
             
-            if(previewModelPrefab != null)
-                previewModelPrefab.SetActive(true);
+            if(character is null) {
+                _previewModelGameObjectNone.SetActive(true);
+            } else {
+                _proceduralSpawnedPreviewModel = Instantiate(character.Model, transform);
+            }
         }
         
     }
