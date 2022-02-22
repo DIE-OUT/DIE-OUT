34678475900d73f7c76bca6bc75f433e94005b04
Afired <conor.huismann@gmail.com>
2022-02-22 14:34:54 +0100

    added startup screen

---
+++ Assets/src/internal/DieOut/UI/ControllerRecommendation/ControllerRecommendationScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DieOut.UI.ControllerRecommendation {
+    
+    public class ControllerRecommendationScreen : MonoBehaviour {
+
+        [SerializeField] private float _timeToWaitBeforeLoadingMainMenu = 5f;
+        [SerializeField] private SceneField _mainMenuScene;
+        
+        private void Start() {
+            StartCoroutine(LoadMainMenuWithDelay(_timeToWaitBeforeLoadingMainMenu));
+        }
+
+        private IEnumerator LoadMainMenuWithDelay(float delay) {
+            yield return new WaitForSeconds(delay);
+            SceneManager.LoadScene(_mainMenuScene.SceneName);
+        }
+        
+    }
+    
+}
--- Assets/src/internal/StartUp.cs
+++ Assets/src/internal/StartUp.cs
@@ -1,19 +1,23 @@
-﻿using Afired.SceneManagement;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace DieOut {
     
     public class StartUp : MonoBehaviour {
-        
+
+        [SerializeField] private bool _skipControllerRecommendationsInEditor = true;
         [SerializeField] private SceneField _mainMenuScene;
+        [SerializeField] private SceneField _controllerRecommendationScene;
         public static bool HasBeenLoaded { get; private set; }
         
         private void Start() {
             HasBeenLoaded = true;
-            SceneManager.LoadScenesAsync(new string[] { _mainMenuScene.SceneName });
+            
             #if UNITY_EDITOR
+            SceneManager.LoadScene(_skipControllerRecommendationsInEditor ? _mainMenuScene.SceneName : _controllerRecommendationScene.SceneName);
             #else
             Application.targetFrameRate = -1;
+            SceneManager.LoadScene(_controllerRecommendationScene.SceneName);
             #endif
         }
         
